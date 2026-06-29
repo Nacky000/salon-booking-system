bc378f586a92b71d07b03dc41418e904430106cc
mokumoku1227 <youohtsuk2005@gmail.com>
2026-06-29 14:01:23 +0900

    ⑦⑨⑩のデータ構造定義

---
+++ app/backend/models/user.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    """会員情報"""
+
+    id: int                 # 会員ID
+    name: str               # 名前
+    email: str              # メールアドレス
+    phone: str              # 電話番号
+    password: str           # パスワード
+    role: str = "customer"  
