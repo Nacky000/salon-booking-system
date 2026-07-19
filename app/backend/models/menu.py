89273d58ca2a342095cb74fe5899530487735980
Nacky000 <mana.tbk0401-ty@docomo.ne.jp>
2026-07-19 23:21:38 +0900

    Update menu.py

--- app/backend/models/menu.py
+++ app/backend/models/menu.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class Menu:
     """施術メニュー情報"""
 
-    id: int         # メニューID
+    menu_id: int         # メニューID
     name: str       # メニュー名
     price: int      # 料金
     duration: int   # 施術時間（分）
